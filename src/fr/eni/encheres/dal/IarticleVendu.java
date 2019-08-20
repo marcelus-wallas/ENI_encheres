5a73aae6612271f078b7dceb0a6c9a916f79e831
Tristan <tristan.e@caliceadsl.fr>
2019-08-20 11:30:19 +0200

    Interfaces + S utilisateur

--- src/fr/eni/encheres/dal/IarticleVendu.java
+++ src/fr/eni/encheres/dal/IarticleVendu.java
@@ -1,5 +1,15 @@
 package fr.eni.encheres.dal;
 
+import java.util.ArrayList;
+
+import fr.eni.encheres.bo.ArticleVendu;
+
 public interface IarticleVendu {
 
+	public void create(ArticleVendu article_vendu);
+	public ArrayList<ArticleVendu> read();
+	public void update(ArticleVendu article_vendu);
+	public void delete(ArticleVendu article_vendu);
+
+	
 }
--- src/fr/eni/encheres/dal/Iretrait.java
+++ src/fr/eni/encheres/dal/Iretrait.java
@@ -1,5 +1,17 @@
 package fr.eni.encheres.dal;
 
+import java.util.ArrayList;
+
+import fr.eni.encheres.bo.Retrait;
+
 public interface Iretrait {
 
+	public void create(Retrait retrait);
+
+	public ArrayList<Retrait> read();
+
+	public void update(Retrait retrait);
+
+	public void delete(Retrait retrait);
+	
 }
