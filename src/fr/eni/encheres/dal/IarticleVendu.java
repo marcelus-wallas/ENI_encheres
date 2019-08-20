b6926d7308af52ea1327406007901f41cf787764
Tristan <tristan.e@caliceadsl.fr>
2019-08-20 11:47:22 +0200

    test

--- src/fr/eni/encheres/dal/IarticleVendu.java
+++ src/fr/eni/encheres/dal/IarticleVendu.java
@@ -7,9 +7,11 @@ import fr.eni.encheres.bo.ArticleVendu;
 public interface IarticleVendu {
 
 	public void create(ArticleVendu article_vendu);
+
 	public ArrayList<ArticleVendu> read();
+
 	public void update(ArticleVendu article_vendu);
+
 	public void delete(ArticleVendu article_vendu);
 
-	
 }
