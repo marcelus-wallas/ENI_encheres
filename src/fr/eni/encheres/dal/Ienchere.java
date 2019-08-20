a2da60dc8be80ad7d83d03f1e4d462263a261042
Tristan <tristan.e@caliceadsl.fr>
2019-08-20 11:39:52 +0200

    ctrl + s sur les encheres !!!

--- src/fr/eni/encheres/dal/Ienchere.java
+++ src/fr/eni/encheres/dal/Ienchere.java
@@ -1,5 +1,17 @@
 package fr.eni.encheres.dal;
 
+import java.util.ArrayList;
+
+import fr.eni.encheres.bo.Enchere;
+
 public interface Ienchere {
+	
+	public void create(Enchere enchere);
+
+	public ArrayList<Enchere> read();
+
+	public void update(Enchere enchere);
+
+	public void delete(Enchere enchere);
 
 }
