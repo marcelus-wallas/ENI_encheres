b9cda16d8f3ebd28a4b1378dba8a297617852f8c
Tristan <tristan.e@caliceadsl.fr>
2019-08-20 11:42:58 +0200

    merge dao to master

--- src/fr/eni/encheres/dal/IarticleVendu.java
+++ src/fr/eni/encheres/dal/IarticleVendu.java
@@ -7,9 +7,10 @@ import fr.eni.encheres.bo.ArticleVendu;
 public interface IarticleVendu {
 
 	public void create(ArticleVendu article_vendu);
-	public ArrayList<ArticleVendu> read();
-	public void update(ArticleVendu article_vendu);
-	public void delete(ArticleVendu article_vendu);
 
-	
+	public ArrayList<ArticleVendu> read();
+
+	public void update(ArticleVendu article_vendu);
+
+	public void delete(ArticleVendu article_vendu);
 }
