ee891038b0799b3b5e8aae271973e4ef2972b23b
max <maxim.bedier2016@campus-eni.fr>
2019-08-23 10:15:55 +0200

    Inscription (En cours)

--- src/fr/eni/encheres/restControlersAPI/Inscription.java
+++ src/fr/eni/encheres/restControlersAPI/Inscription.java
@@ -30,58 +30,55 @@ public class Inscription {
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response inscription(InscriptionDTO inscriptionDTO) {
 		
-		if(inscriptionDTO.getMot_de_passe() == inscriptionDTO.getConfirmation())
-		{
-			Response response = null;
-			
-			Utilisateur temp = new Utilisateur(inscriptionDTO.getPseudo(),
-												inscriptionDTO.getNom(),
-												inscriptionDTO.getPrenom(),
-												inscriptionDTO.getEmail(),
-												inscriptionDTO.getTelephone(),
-												inscriptionDTO.getRue(),
-												inscriptionDTO.getCode_postal(),
-												inscriptionDTO.getVille(),
-												inscriptionDTO.getMot_de_passe(),
-												200,
-												false);
-			
-			 ArrayList<Utilisateur> listeUtilisateurs = connexionBdd.read();
-			 for(Utilisateur user : listeUtilisateurs)
+		Response response = null;
+		
+		Utilisateur temp = new Utilisateur(inscriptionDTO.getPseudo(),
+											inscriptionDTO.getNom(),
+											inscriptionDTO.getPrenom(),
+											inscriptionDTO.getEmail(),
+											inscriptionDTO.getTelephone(),
+											inscriptionDTO.getRue(),
+											inscriptionDTO.getCode_postal(),
+											inscriptionDTO.getVille(),
+											inscriptionDTO.getMot_de_passe(),
+											200,
+											false);
+//		if(temp.getMot_de_passe() != inscriptionDTO.getConfirmation())
+//		{
+//			//Mot de passe != Confirmation
+//			etat = "Password KO";
+//			System.out.println(etat);
+//			return Response.status(Status.BAD_REQUEST).build();
+//		}
+		
+		 ArrayList<Utilisateur> listeUtilisateurs = connexionBdd.read();
+		 for(Utilisateur user : listeUtilisateurs)
+		 {
+			 if(user.getPseudo() == temp.getPseudo())
 			 {
-				 if(user.getPseudo() == inscriptionDTO.getPseudo())
-				 {
-					 //Pseudo existant
-					 etat = "Pseudo KO";
-					 System.out.println(etat);
-					 response = Response.status(Status.CONFLICT).build();
-				 }
-				 if(user.getEmail() == inscriptionDTO.getEmail())
-				 {
-					 //Email existant
-					 etat = "Email KO";
-					 System.out.println(etat);
-					 response = Response.status(Status.CONFLICT).build();
-				 }
-				 if(user.getPseudo() != inscriptionDTO.getPseudo() && user.getEmail() != inscriptionDTO.getEmail())
-				 {
-					 //Création de l'utilisateur
-					 etat = "Création OK";
-					 System.out.println(etat);
-					 connexionBdd.create(temp);	
-					 response =  Response.ok().build();
-				 }
+				 //Pseudo existant
+				 etat = "Pseudo KO";
+				 System.out.println(etat);
+				 response = Response.status(Status.CONFLICT).build();
 			 }
-			 
-			 return response;
-			
-		}else {
-			//Mot de passe != Confirmation
-			etat = "Password KO";
-			 System.out.println(etat);
-			return Response.status(Status.BAD_REQUEST).build();
-		}
-
+			 if(user.getEmail() == temp.getEmail())
+			 {
+				 //Email existant
+				 etat = "Email KO";
+				 System.out.println(etat);
+				 response = Response.status(Status.CONFLICT).build();
+			 }
+			 if(user.getPseudo() != temp.getPseudo() && user.getEmail() != temp.getEmail())
+			 {
+				 //Création de l'utilisateur
+				 etat = "Création OK";
+				 System.out.println(etat);
+				 connexionBdd.create(temp);	
+				 response =  Response.ok().build();
+			 }
+		 }
+		 
+		 return response;
 		
 	}		
 }
