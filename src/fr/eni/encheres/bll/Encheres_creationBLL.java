ee03f7cf06ba0ee83751a3844640bfa584c56f1b
Tristan <tristan.e@caliceadsl.fr>
2019-08-28 09:34:44 +0200

    ENCHERES/CREATION - retrait non renseigne = adresse utilisateur

--- src/fr/eni/encheres/bll/Encheres_creationBLL.java
+++ src/fr/eni/encheres/bll/Encheres_creationBLL.java
@@ -17,26 +17,23 @@ public class Encheres_creationBLL {
 	public Encheres_creationBLL() {
 		this.articleVenduDAL = Factory.getArticleVendu();
 		this.retraitDAL = Factory.getRetrait();
-		this.utilisateurDAL =  Factory.getUtilisateur();
+		this.utilisateurDAL = Factory.getUtilisateur();
 	}
 
-	
-	
 	public ArticleVendu newArticle(ArticleVendu i) {
 		return articleVenduDAL.create(i);
 	}
 
 	public void newRetrait(Retrait retrait, Utilisateur utilisateur) {
-		/*Retrait retraitTemp = null;
-		if (retrait.getRue() == null ) {
-			
-			utilisateurDAL.readUser(utilisateur.getNo_utilisateur());
-			retraitTemp = new Retrait(utilisateur.getRue(), utilisateur.getCode_postal(), utilisateur.getVille());
+		Retrait retraitTemp = null;
+		if (retrait.getRue() == null || retrait.getCode_postal() == null || retrait.getVille() == null) {
+			Utilisateur user = utilisateurDAL.readUser(utilisateur.getNo_utilisateur());
+			retraitTemp = new Retrait(retrait.getNo_article(), user.getRue(), user.getCode_postal(), user.getVille());
+			System.out.println("nom" + retraitTemp);
 		} else {
-			retraitTemp = new Retrait(retrait.getRue(), retrait.getCode_postal(), retrait.getVille());
+			retraitTemp = new Retrait(retrait.getNo_article(), retrait.getRue(), retrait.getCode_postal(),
+					retrait.getVille());
 		}
 		this.retraitDAL.create(retraitTemp);
-		*/
-		this.retraitDAL.create(retrait);
-	}	
+	}
 }
